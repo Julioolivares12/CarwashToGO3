6395338df8f8ea1cc0824a9b8eb9460f1579ee86
julio <julioolivares90@gmail.com>
2019-09-23 17:11:47 -0600

    arreglado error en RecyclerViewAdapter

--- app/src/main/java/com/julio/carwashtogo3/ui/administrador/promocion/ListarPromociones.java
+++ app/src/main/java/com/julio/carwashtogo3/ui/administrador/promocion/ListarPromociones.java
@@ -15,7 +15,7 @@ import com.julio.carwashtogo3.R;
 import com.julio.carwashtogo3.common.Constantes;
 
 public class ListarPromociones extends Fragment {
-    private OnFragmentInteractionListener mListener;
+   
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference refPromocion = database.getReference(Constantes.REF_PROMOCIONES);
     private StorageReference storageReference;
@@ -34,33 +34,4 @@ public class ListarPromociones extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
