629391cca1b856a3e02e533ac9607b88feff2005
julio <julioolivares90@gmail.com>
2019-09-23 21:43:23 -0600

    solucion a errores

--- app/src/main/java/com/julio/carwashtogo3/ui/administrador/empresa/ListarEmpresaFragment.java
+++ app/src/main/java/com/julio/carwashtogo3/ui/administrador/empresa/ListarEmpresaFragment.java
@@ -2,14 +2,22 @@ package com.julio.carwashtogo3.ui.administrador.empresa;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.navigation.Navigation;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.julio.carwashtogo3.R;
 import com.julio.carwashtogo3.adapters.EmpresaRecyclerViewAdapter;
 import com.julio.carwashtogo3.common.Constantes;
@@ -27,6 +35,9 @@ public class ListarEmpresaFragment extends Fragment {
 
     private boolean isTwoPane=false;
     private List<Empresa> empresaList = new ArrayList<>();
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference empresasRef;
+
     public ListarEmpresaFragment() {
         // Required empty public constructor
     }
@@ -37,12 +48,13 @@ public class ListarEmpresaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_listar_empresa, container, false);
-
+        empresasRef = firebaseDatabase.getReference(Constantes.REF_EMPRESA);
         if (view.findViewById(R.id.detalle_empresa)!= null){
             isTwoPane = true;
         }
         RecyclerView recyclerView =view.findViewById(R.id.rb_empresas_list);
 
+        getListaEmpresas();
         setRecyclerView(recyclerView);
         return view;
     }
@@ -75,7 +87,24 @@ public class ListarEmpresaFragment extends Fragment {
     }
 
     private void getListaEmpresas(){
-        
+        //para leer datos
+        empresasRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        Empresa empresa = snapshot.getValue(Empresa.class);
+                        empresaList.add(empresa);
+                    }
+                    Log.d("empresa list",empresaList.get(0).getNombreEmpresa());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), "ocurrio un error "+databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
