b2cb38ddbae944ece9ab760d9b69a11676de9a50
Rebeca Orellana <saraiorellana91@gmail.com>
2019-10-08 23:01:25 -0600

    se agrego diseño de lista de paqueto/ falta codigo

--- app/src/main/java/com/julio/carwashtogo3/ui/administrador/empresa/ListarEmpresaFragment.java
+++ app/src/main/java/com/julio/carwashtogo3/ui/administrador/empresa/ListarEmpresaFragment.java
@@ -43,6 +43,7 @@ public class ListarEmpresaFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
