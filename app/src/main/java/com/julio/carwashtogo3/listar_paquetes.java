09403c3c651ea000a6beed6b4d56176bf548ff89
Rebeca Orellana <saraiorellana91@gmail.com>
2019-10-08 22:39:52 -0600

    se agrego diseño de lista de paqueto/ falta codigo

---
+++ app/src/main/java/com/julio/carwashtogo3/listar_paquetes.java
@@ -0,0 +1,14 @@
+package com.julio.carwashtogo3;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class listar_paquetes extends Fragment {
+
+}
