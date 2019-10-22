db12939d3185dbd8e4560c213b373e264541afc6
julio <julioolivares90@gmail.com>
2019-10-22 00:21:50 -0600

    compra

--- app/src/main/java/com/julio/carwashtogo3/model/CompraPaquete.java
+++ app/src/main/java/com/julio/carwashtogo3/model/CompraPaquete.java
@@ -1,8 +1,69 @@
 package com.julio.carwashtogo3.model;
 
 public class CompraPaquete {
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    private String UID;
     private String titulo;
     private double precio;
     private String nombreCliente;
+    private String descripcion;
 
+    public String getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(String fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    private String fechaReserva;
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    private String imagen;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
 }
--- app/src/main/java/com/julio/carwashtogo3/ui/cliente/catalogo/DetalleProductoFragment.java
+++ app/src/main/java/com/julio/carwashtogo3/ui/cliente/catalogo/DetalleProductoFragment.java
@@ -2,9 +2,12 @@ package com.julio.carwashtogo3.ui.cliente.catalogo;
 
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.PluralsRes;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
@@ -19,6 +22,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,7 +33,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.julio.carwashtogo3.R;
 import com.julio.carwashtogo3.common.Constantes;
 import com.julio.carwashtogo3.common.DatePickerFragment;
+import com.julio.carwashtogo3.model.CompraPaquete;
 import com.julio.carwashtogo3.model.Paquete;
+import com.julio.carwashtogo3.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,12 +52,24 @@ public class DetalleProductoFragment extends Fragment {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference reference;
     private DatabaseReference paqueteRef;
+    private FirebaseAuth mAuth;
 
+    private String urlImagen;
+
+    private DatabaseReference refUsuarios;
+    ProgressDialog progressDialog;
     public DetalleProductoFragment() {
         // Required empty public constructor
     }
 
 
+    public void setUrlImagen(String urlImagen){
+        this.urlImagen = urlImagen;
+    }
+
+    public String getUrlImagen(){
+        return urlImagen;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -72,6 +92,8 @@ public class DetalleProductoFragment extends Fragment {
         firebaseDatabase = FirebaseDatabase.getInstance ();
         reference = firebaseDatabase.getReference(Constantes.REF_COMPRAS);
         paqueteRef= firebaseDatabase.getReference ( Constantes.REF_PAQUETES );
+        refUsuarios = firebaseDatabase.getReference ( Constantes.REF_USUARIOS );
+        mAuth = FirebaseAuth.getInstance ();
         if (getArguments ()!= null){
            final String UID_PRODUCTO = getArguments ().getString (Constantes.UID_PAQUETE);
             paqueteRef.child (UID_PRODUCTO).addValueEventListener ( new ValueEventListener () {
@@ -87,6 +109,7 @@ public class DetalleProductoFragment extends Fragment {
                             if (paquete.getUrlImagen () != null && !TextUtils.isEmpty(paquete.getUrlImagen())){
                                 View view1 = getView ();
                                 assert view1!= null;
+                                setUrlImagen ( paquete.getUrlImagen () );
                                 Glide.with (view1).load(paquete.getUrlImagen()).centerCrop().into (iv_compra );
                             }
 
@@ -113,11 +136,57 @@ public class DetalleProductoFragment extends Fragment {
         btnComprar.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                
+
+
+                progressDialog = new ProgressDialog (getContext ());
+                progressDialog.setMessage ( "PROCESANDO COMPRA" );
+                progressDialog.setIndeterminate ( true );
+                progressDialog.setCancelable ( false );
+                progressDialog.show ();
+               final String key= reference.push ().getKey ();
+               final String title = tv_titulo_compra.getText ().toString ();
+               final String descripcion = tv_descripcion_compra.getText ().toString ();
+               final String fecha = fechaCompra.getText ().toString ();
+
+                FirebaseUser usuarioActual = mAuth.getCurrentUser ();
+
+                refUsuarios.child ( usuarioActual.getUid () ).addValueEventListener ( new ValueEventListener () {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists ()){
+                               User user = dataSnapshot.getValue (User.class);
+                               CompraPaquete compraPaquete = new CompraPaquete ();
+                               compraPaquete.setUID(key);
+                               compraPaquete.setTitulo(title);
+                               compraPaquete.setDescripcion(descripcion);
+                               compraPaquete.setFechaReserva(fecha);
+                               compraPaquete.setImagen (getUrlImagen ());
+                               compraPaquete.setNombreCliente (user.getNombre ());
+                               comPrar(compraPaquete);
+                               progressDialog.dismiss ();
+                            }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        progressDialog.dismiss ();
+                    }
+                } );
             }
         } );
     }
 
+    private void comPrar(CompraPaquete compraPaquete){
+        reference.child (compraPaquete.getUID ()).setValue ( compraPaquete ).addOnSuccessListener ( new OnSuccessListener<Void> () {
+            @Override
+            public void onSuccess(Void aVoid) {
+                View view = getView ();
+                assert view != null;
+                Snackbar.make ( view,"Compra realizada con exito",Snackbar.LENGTH_LONG ).show ();
+            }
+        } );
+
+    }
     private void showDateDialog() {
         DatePickerFragment datePickerFragment = DatePickerFragment.newInstance ( new DatePickerDialog.OnDateSetListener () {
             @Override
